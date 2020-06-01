647924797491ef0eaad3ef10cf4c6666ebf98f98
xalbi <ahmad.chelbi@gmail.com>
2020-06-01 21:23:45 +0200

    add moment to dashboard module

--- client/src/app/dashboard/dashboard.module.ts
+++ client/src/app/dashboard/dashboard.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { JobsComponent } from './jobs/jobs.component';
 import { JobsService } from './jobs.service';
 import { MaterialModule } from 'src/material.module';
+import { MomentModule } from 'ngx-moment';
 
 @NgModule({
 	declarations: [JobsComponent],
 	imports: [
+		MomentModule,
 		CommonModule,
 		MaterialModule
 	],
