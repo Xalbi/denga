1ea066d62aa655d1a374d6cae01b2034c11cd42b
xalbi <ahmad.chelbi@gmail.com>
2020-06-01 21:25:46 +0200

    get collection name from config

--- server/src/databases/mongodb.ts
+++ server/src/databases/mongodb.ts
@@ -1,13 +1,15 @@
 import { MongoClient, Db, MongoClientOptions, Collection } from "mongodb";
+import * as get_settings from '../get_settings';
+const settings = get_settings.json()
 
 let db: Db;
 
 export async function connect(url, options?: MongoClientOptions) {
     db = await MongoClient.connect(url, options);
+    db = db.db(db.s.options.dbName);
     console.log("Connected successfully to mongodb");
-    db = db.db('octopoda');
 }
 
 export function JobsDB(): Collection {
-    return db.collection('agenda_jobs');
+    return db.collection(settings.collection);
 }
