4c18d04c14c72aa85d7c48f7b5a8e274d3158d59
xalbi <ahmad.chelbi@gmail.com>
2020-06-01 21:24:37 +0200

    adapt Mat datasource to job object

--- client/src/app/dashboard/jobs/jobs.component.ts
+++ client/src/app/dashboard/jobs/jobs.component.ts
@@ -1,30 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import {SelectionModel} from '@angular/cdk/collections';
-import {MatTableDataSource} from '@angular/material/table';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatTableDataSource, MatTable } from '@angular/material/table';
 import { JobsService } from '../jobs.service';
+import { Subscription, interval } from 'rxjs';
 
-
-export interface PeriodicElement {
-	name: string;
-	position: number;
-	weight: number;
-	symbol: string;
-  }
-  
-  const ELEMENT_DATA: PeriodicElement[] = [
-	{position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-	{position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-	{position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-	{position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-	{position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-	{position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-	{position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-	{position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-	{position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-	{position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  ];
-  
+export interface Job {
+	name: string,
+	data: any,
+	type: string,
+	priority: number,
+	repeatInterval: string,
+	nextRunAt: Date,
+	lastRunAt: Date,
+	lastFinishedAt: Date,
+	lockedAt: Date,
+	repeatTimezone: any,
+	lastModifiedBy: any,
+	failCount?: number,
+	failReason?: string,
+	failedAt?: Date
+}
 
 @Component({
 	selector: 'app-jobs',
@@ -32,43 +27,56 @@ export interface PeriodicElement {
 	styleUrls: ['./jobs.component.scss']
 })
 export class JobsComponent implements OnInit {
-	jobs = []
-
-	displayedColumns: string[] = ['select', 'position', 'name', 'weight', 'symbol'];
-	dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-	selection = new SelectionModel<PeriodicElement>(true, []);
-  
-	/** Whether the number of selected elements matches the total number of rows. */
-	isAllSelected() {
-	  const numSelected = this.selection.selected.length;
-	  const numRows = this.dataSource.data.length;
-	  return numSelected === numRows;
-	}
-  
-	/** Selects all rows if they are not all selected; otherwise clear selection. */
-	masterToggle() {
-	  this.isAllSelected() ?
-		  this.selection.clear() :
-		  this.dataSource.data.forEach(row => this.selection.select(row));
-	}
-  
-	/** The label for the checkbox on the passed row */
-	checkboxLabel(row?: PeriodicElement): string {
-	  if (!row) {
-		return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
-	  }
-	  return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
-	}
-
+	JOB_DATA: Job[] = []
+	dataSource
+	displayedColumns: string[] = ['name', 'type', 'lastRunAt', 'nextRunAt', 'repeatInterval'];
+	selection = new SelectionModel<Job>(true, []);
+	@ViewChild(MatTable) table: MatTable<any>;
+	private refresh: Subscription;
 	constructor(
 		private jobsService: JobsService,
 	) { }
 
 	async ngOnInit() {
-		await this.init()
+		this.refresh = interval(2000).subscribe(async (val) => {
+			try {
+				await this.init()
+			} catch (error) {
+				/**
+				 * lost server cnx
+				 */
+			}
+		})
 	}
 
 	async init() {
-		this.jobs = await this.jobsService.getJobs().toPromise()
+		this.JOB_DATA = await this.jobsService.getJobs().toPromise()
+		this.dataSource = new MatTableDataSource<Job>(this.JOB_DATA);
 	}
+
+
+
+	/** Whether the number of selected elements matches the total number of rows. */
+	isAllSelected() {
+		const numSelected = this.selection.selected.length;
+		const numRows = this.dataSource.data.length;
+		return numSelected === numRows;
+	}
+
+	/** Selects all rows if they are not all selected; otherwise clear selection. */
+	masterToggle() {
+		this.isAllSelected() ?
+			this.selection.clear() :
+			this.dataSource.data.forEach(row => this.selection.select(row));
+	}
+
+	/** The label for the checkbox on the passed row */
+	checkboxLabel(row?: Job): string {
+		if (!row) {
+			return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+		}
+		return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.name + 1}`;
+	}
+
+
 }
