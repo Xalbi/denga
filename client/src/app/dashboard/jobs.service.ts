af6c15ce06a892a2e4f7f62cb0764819d6dfdb54
xalbi <ahmad.chelbi@gmail.com>
2020-06-06 15:06:06 +0200

    get api url in runtime: new conf system

--- client/src/app/dashboard/jobs.service.ts
+++ client/src/app/dashboard/jobs.service.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
 
 @Injectable()
 export class JobsService {
-
-	url = environment.api + '/jobs/';
+	host = `${window.location.protocol}//${window.location.host}`
+	url = this.host + '/api/jobs/';
 	constructor(private http: HttpClient) { }
-
+	
 	getJobs():Observable<any> {
+		console.log(this.host);
 		return this.http.get(this.url + 'all')
 	}
-
 }
