08910a4e398f3dac72e1194d406a7c261175bde6
xalbi <ahmad.chelbi@gmail.com>
2020-06-06 15:09:25 +0200

    limit job number with new conf system

--- server/src/libs/jobs.lib.ts
+++ server/src/libs/jobs.lib.ts
@@ -1,16 +1,17 @@
 import { JobsDB } from '../databases/mongodb';
-
+const Config = require('conf');
+const settings = new Config();
 export class JobsLib {
 
     async getAll(){        
         try {
-            let res = await JobsDB().find({}).toArray()
+            let res = await JobsDB().find({}).limit(settings.get('limit')).toArray()
             res = res.map(c => {
                 c._id = c._id.toString()
                 return c
             })
             return res           
-        } catch (error) {
+        } catch (error) {            
             throw error;
         }    
     }
