d0daecf36bcfe58d9e154c3818f29850819c9e0b
xalbi <ahmad.chelbi@gmail.com>
2020-06-14 17:17:57 +0200

    remove and requeue endpoints (ctrl)

--- server/src/controllers/jobs.controller.ts
+++ server/src/controllers/jobs.controller.ts
@@ -1,4 +1,4 @@
-import { JsonController, Get} from "routing-controllers";
+import { JsonController, Get, Post, Body} from "routing-controllers";
 import { JobsLib } from "../libs/jobs.lib";
  
 @JsonController('/jobs')
@@ -6,8 +6,24 @@ import { JobsLib } from "../libs/jobs.lib";
 export class CompanyController {
     jobsLib = new JobsLib();
 
+    /**
+    TODO:
+    make sure that the user filter will be used after any query
+    after a delete or q requeue we must get the new list filtred
+     */
+
     @Get("/all")
     async getAll() {
         return await this.jobsLib.getAll()
+    }
+   
+    @Post("/remove")
+    async remove(@Body() body: any) {
+        return await this.jobsLib.remove(body)
+    }
+
+    @Post("/requeue")
+    async requeue(@Body() body: any) {
+        return await this.jobsLib.requeue(body)
     }
 }
