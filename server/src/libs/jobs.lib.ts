326796f6fc39565bb388baec87e42068b31b0710
xalbi <ahmad.chelbi@gmail.com>
2020-06-20 22:54:49 +0200

    create dbFilter (indent)

--- server/src/libs/jobs.lib.ts
+++ server/src/libs/jobs.lib.ts
@@ -1,5 +1,6 @@
 import { JobsDB } from '../databases/mongodb';
 import { ObjectId } from 'mongodb';
+import { Filter } from '../models/filter';
 const agenda = require('./agenda.lib');
 
 
@@ -7,41 +8,53 @@ const Config = require('conf');
 const settings = new Config();
 export class JobsLib {
 
-    async getAll(){        
-        try {
-            let res = await JobsDB().find({}).limit(settings.get('limit')).toArray()
-            res = res.map(c => {
-                c._id = c._id.toString()
-                return c
-            })
-            return res           
-        } catch (error) {            
-            throw error;
-        }    
-    }
+	async getAll(filter: Filter) {
+		let dbFilter: any = {};
+		if (filter.search_string) {
+			dbFilter = { $and: [] }
+			dbFilter.$and.push({
+				name:
+				{
+					$regex: filter.search_string,
+					$options: 'i'
+				}
+			})
+		}
 
-    async remove(param){
-        let idsList = param.ids.map(i => new ObjectId(i))     
-        try {
-            let res = await JobsDB().deleteMany({
-                _id: {$in: idsList}
-            });
-            return res           
-        } catch (error) {            
-            throw error;
-        }    
-    }
+		try {
+			let res = await JobsDB().find(dbFilter).limit(settings.get('limit')).toArray()
+			res = res.map(c => {
+				c._id = c._id.toString()
+				return c
+			})
+			return res
+		} catch (error) {
+			throw error;
+		}
+	}
 
-    async requeue(param){
-        let idsList = param.ids.map(i => new ObjectId(i))     
-        let jobs = await JobsDB().find({
-            _id: {$in: idsList}
-        }).toArray();
+	async remove(param) {
+		let idsList = param.ids.map(i => new ObjectId(i))
+		try {
+			let res = await JobsDB().deleteMany({
+				_id: { $in: idsList }
+			});
+			return res
+		} catch (error) {
+			throw error;
+		}
+	}
 
-        for (const job of jobs) {
-            agenda.now(job.name, job.data)
-        }
-        return {}
-    }
+	async requeue(param) {
+		let idsList = param.ids.map(i => new ObjectId(i))
+		let jobs = await JobsDB().find({
+			_id: { $in: idsList }
+		}).toArray();
+
+		for (const job of jobs) {
+			agenda.now(job.name, job.data)
+		}
+		return {}
+	}
 
 }
