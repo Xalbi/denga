99dd346e82273ff418b16a22753e6a5f86115960
xalbi <ahmad.chelbi@gmail.com>
2024-04-03 16:15:45 +0200

    detect the installed version of Node.js and inform the user about the required version without raising an error

--- bin/cli.js
+++ bin/cli.js
@@ -3,6 +3,18 @@
 const meow = require('meow');
 const Config = require('conf');
 
+const requiredVersion = "10.15.0"; // Example required version
+
+// Check if the installed version meets the requirements
+if (parseFloat(process.version.slice(1)) < parseFloat(requiredVersion)) {
+    console.log(`You need to upgrade Node.js to version ${requiredVersion} or higher.`);
+    console.log(`Installed version: ${process.version}`);
+	process.exit()
+} else {
+    console.log(`Your Node.js version ${process.version} meets the requirements.`);
+    // Your application code goes here
+}
+
 const cli = meow(`
 	Usage
 	  $ denga --db mongodb://127.0.0.1:27017/denga 
